369844b9107489003d81e3aacc331afbdd2b23c2
Kakolay Nimalov <47940537+SplashTheBatya@users.noreply.github.com>
2021-05-27 00:33:01 +0300

    Updated docker-compose.yml and started models.py by setting Offer and Response models

--- freelance_app/models.py
+++ freelance_app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.conf import settings
+from django.db.models import CASCADE
 
-# Create your models here.
+
+class OfferStatusChoices(models.TextChoices):
+    NEW = "NEW", "Новый"
+    IN_PROGRESS = "IN_PROGRESS", "В процессе"
+    DONE = "DONE", "Выполнен"
+
+
+class Offer(models.Model):
+    customer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=CASCADE
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(
+        choices=OfferStatusChoices.choices,
+        default=OfferStatusChoices.NEW,
+        max_length=255
+    )
+    payment = models.PositiveIntegerField()
+
+
+class Response(models.Model):
+    freelancer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=CASCADE
+    )
+    commentary = models.TextField()
+
+
+class ResponseImage(models.Model):
+    response = models.ForeignKey(Response, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+    default = models.BooleanField(default=False)
+
--- freelance_market/settings.py
+++ freelance_market/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'freelance_app',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +81,12 @@ WSGI_APPLICATION = 'freelance_market.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB', default='freelance_market'),
+        'USER': os.environ.get('POSTGRES_USER', default='user'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', default='user'),
+        'HOST': os.environ.get('POSTGRES_HOST', default='localhost'),
+        'PORT': "5432"
     }
 }
 
